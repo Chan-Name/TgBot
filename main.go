5c7f8d7a4a38f146588648f179ce2c5ff46ac68d
Chan-Name <ivan.shakhov.08@gmail.com>
2024-10-28 20:39:12 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"TG/pkg/telegram"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
 	bot, _ := tgbotapi.NewBotAPI(jsonTokenDecoder())
 
-	b := telegram.NewBot(bot)
+	b := NewBot(bot)
 	b.Start()
 }
